685670501ff50e6db816743c9f1b2e731c7f53fe
tecnolisolutions <tecnoli.solutions@outlook.com>
2022-04-17 08:31:24 -0600

    se actualizo y se probo el archivo main con referencia a la clase Interfaz Epersona

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from src.logica.Logica_mock import Logica_mock
+from src.vista.InterfazEpersona import App_EPersona
 
 if __name__ == '__main__':
     # Punto inicial de la aplicación
@@ -8,3 +9,6 @@ if __name__ == '__main__':
     logica = Logica_mock()
     print(logica.texto)
     print(logica.personas)
+
+    app = App_EPersona(sys.argv, logica)
+

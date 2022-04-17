29e3b5e97a5ea1ad6d187c0217c8586f95848a69
tecnolisolutions <tecnoli.solutions@outlook.com>
2022-04-17 08:30:17 -0600

    se crea la clase vista\InterfazEpersona

---
+++ src/vista/InterfazEpersona.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QApplication
+
+class App_EPersona(QApplication):
+    """
+    Clase principal de la interfaz que coordina las diferentes vistas/ventanas de la aplicación
+    """
+
+    def __init__(self, sys_argv, logica):
+        """
+        Constructor de la interfaz. Debe recibir la lógica e iniciar la aplicación en la ventana principal.
+        """
+        super(App_EPersona, self).__init__(sys_argv)
+
+        self.logica = logica
+        self.mostrar_vista_lista_personas()

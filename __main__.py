dd83ab6f44ec69bca2abd46df7bf507f2234ec08
tecnolisolutions <tecnoli.solutions@outlook.com>
2022-04-17 08:02:41 -0600

    se actualizo y se probo el archivo main con referencia a la clase Logica_mock

--- __main__.py
+++ __main__.py
@@ -1,5 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from src.logica.Logica_mock import Logica_mock
 
 if __name__ == '__main__':
-# Punto inicial de la aplicación
+    # Punto inicial de la aplicación
+
+    logica = Logica_mock()
+    print(logica.texto)
+    print(logica.personas)

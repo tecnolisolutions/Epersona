50aca46c58ed509e72481225e5903a3d31011959
tecnolisolutions <tecnoli.solutions@outlook.com>
2022-04-17 08:01:34 -0600

    se creo el archivo Logica_mock

---
+++ src/logica/Logica_mock.py
@@ -0,0 +1,13 @@
+'''
+Esta clase es tan sólo un mock con datos para probar la interfaz
+'''
+class Logica_mock():
+
+    def __init__(self):
+        # Este constructor contiene los datos falsos para probar la interfaz
+        self.texto="Hola Mundo"
+        self.personas=[{'Nombre':'Julio', 'Apellido':'Serrano'},\
+                       {'Nombre':'Oliver', 'Apellido':'Serrano'},\
+                       {'Nombre':'Caro', 'Apellido':'Villeda'},\
+                       {'Nombre':'olivia', 'Apellido':'Soria'}]
+
